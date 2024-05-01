200dd4bee6618c132a42c9153440876c68e91340
artichaudhary <chaudhary.aarti1998@gmail.com>
2024-05-01 10:02:26 +0530

    increment, decrement and theme change

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/route_manager.dart';
 
 void main() {
@@ -49,112 +50,98 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    CountController controller =
+        Get.put(CountController()); // dependency injection
+
     return PopScope(
-        canPop: isBack,
-        onPopInvoked: (bool sajjan) {
-          if (sajjan) {
-            return;
-          }
-          Get.defaultDialog(
-              title: "Exit",
-              content: Text("Are you sure"),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                    Navigator.pop(context);
-                  },
-                  child: Text("Yes"),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text("No"),
-                ),
-              ]);
-        },
-        child: Scaffold(
-          appBar: AppBar(
-            // TRY THIS: Try changing the color here to a specific color (to
-            // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-            // change color while the other colors stay the same.
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-            // Here we take the value from the MyHomePage object that was created by
-            // the App.build method, and use it to set our appbar title.
-            title: Text(widget.title),
-          ),
-          body: Center(
-            // Center is a layout widget. It takes a single child and positions it
-            // in the middle of the parent.
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'You have pushed the button this many times:',
-                ),
-                Text(
-                  '$_counter',
+      canPop: isBack,
+      onPopInvoked: (bool sajjan) {
+        if (sajjan) {
+          return;
+        }
+        Get.defaultDialog(
+            title: "Exit",
+            content: Text("Are you sure"),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                },
+                child: Text("Yes"),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("No"),
+              ),
+            ]);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Obx(() {
+                return Text(
+                  '${controller.count}',
                   style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ],
-            ),
+                );
+              }),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      controller.decrement();
+                      print(controller.count);
+                    },
+                    tooltip: 'Increment',
+                    child: const Icon(Icons.minimize),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      controller.increment();
+                      print(controller.count);
+                      // Get.changeTheme(ThemeData.light());
+                    },
+                    tooltip: 'Increment',
+                    child: const Icon(Icons.add),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      Get.changeTheme(
+                        Get.isDarkMode ? ThemeData.light() : ThemeData.dark(),
+                      );
+                    },
+                    tooltip: 'Increment',
+                    child: const Icon(Icons.format_paint),
+                  ),
+                ],
+              ),
+            ],
           ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: (){
-              Get.changeTheme(ThemeData.light());
-            },
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ), // This trailing comma makes auto-formatting nicer for build methods.
-        ));
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     // TRY THIS: Try changing the color here to a specific color (to
-    //     // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-    //     // change color while the other colors stay the same.
-    //     backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-    //     // Here we take the value from the MyHomePage object that was created by
-    //     // the App.build method, and use it to set our appbar title.
-    //     title: Text(widget.title),
-    //   ),
-    //   body: Center(
-    //     // Center is a layout widget. It takes a single child and positions it
-    //     // in the middle of the parent.
-    //     child: Column(
-    //       // Column is also a layout widget. It takes a list of children and
-    //       // arranges them vertically. By default, it sizes itself to fit its
-    //       // children horizontally, and tries to be as tall as its parent.
-    //       //
-    //       // Column has various properties to control how it sizes itself and
-    //       // how it positions its children. Here we use mainAxisAlignment to
-    //       // center the children vertically; the main axis here is the vertical
-    //       // axis because Columns are vertical (the cross axis would be
-    //       // horizontal).
-    //       //
-    //       // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-    //       // action in the IDE, or press "p" in the console), to see the
-    //       // wireframe for each widget.
-    //       mainAxisAlignment: MainAxisAlignment.center,
-    //       children: <Widget>[
-    //         const Text(
-    //           'You have pushed the button this many times:',
-    //         ),
-    //         Text(
-    //           '$_counter',
-    //           style: Theme.of(context).textTheme.headlineMedium,
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    //   floatingActionButton: FloatingActionButton(
-    //     onPressed: () {
-    //       // Get.snackbar("hi","bhd",);
-    //
-    //     },
-    //     tooltip: 'Increment',
-    //     child: const Icon(Icons.add),
-    //   ), // This trailing comma makes auto-formatting nicer for build methods.
-    // );
+        ),
+      ),
+    );
+  }
+}
+
+class CountController extends GetxController {
+  var count = 0.obs;
+  RxBool isTrue = true.obs;
+  increment() => count++;
+  decrement() {
+    count--;
   }
 }
